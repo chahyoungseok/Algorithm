81f6af7fb8bad2e3ee14c26bffac24d9e5dbf91a
Cha Hyoung Seok <xcohdsuts@gmail.com>
2023-04-07 17:30:36 +0900

    [Gold IV] Title: 해킹, Time: 1064 ms, Memory: 150740 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,81 +1,122 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int T; //테스트 케이스 수
-    private static int N; //컴퓨터 개수
-    private static int D; //의존성 개수
-    private static int C; //해킹당한 컴퓨터 번호
-    private static int[] distances; // 최단거리
-    private static List<Edge>[] edgeList;
-    private static int cnt = 0; // 총 감염되는 컴퓨터 수
-    private static int time = 0; // 마지막 컴퓨터가 감염되기까지의 걸리는 시간
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        T = sc.nextInt();
-        for (int t = 0; t < T; t++) {
-            N = sc.nextInt();
-            D = sc.nextInt();
-            C = sc.nextInt();
-            distances = new int[N + 1];
-            edgeList = new ArrayList[N + 1];
-            Arrays.fill(distances, Integer.MAX_VALUE);
-            for (int i = 0; i <= N; i++) {
-                edgeList[i] = new ArrayList<>();
+    private static int n,d,c;
+    private final static int INF = (int) 1e9;
+    private static ArrayList<ArrayList<Node>> graph;
+    private static int[] dis = new int[10001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(reader.readLine());
+
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (t-- > 0) {
+            st = new StringTokenizer(reader.readLine());
+
+            n = Integer.parseInt(st.nextToken());//컴퓨터 개수
+            d = Integer.parseInt(st.nextToken());//의존성 개수
+            c = Integer.parseInt(st.nextToken());//해킹당한 컴퓨터 번호
+
+            graph = new ArrayList<>();
+
+            for (int i=0; i<=n; i++) {
+                graph.add(new ArrayList<>());
             }
-            for (int i = 0; i < D; i++) {
-                int a = sc.nextInt(); //컴퓨터 a
-                int b = sc.nextInt(); //컴퓨터 b
-                int s = sc.nextInt(); //S초 후 감염
-                edgeList[b].add(new Edge(a, s)); // 컴퓨터 b가 감염되면 s초 후 컴퓨터 a도 감염됨을 뜻한다. (a, b 거꾸로 주의)
+
+            for (int i=0; i<d; i++) {
+                st = new StringTokenizer(reader.readLine());
+
+                int a = Integer.parseInt(st.nextToken());//컴퓨터 a가
+                int b = Integer.parseInt(st.nextToken());//컴퓨터 b를 의존
+                int s = Integer.parseInt(st.nextToken());//b가 감염되면 s초후 a도 감염됨
+
+                graph.get(b).add(new Node(a, s));
             }
-            distances[C] = 0;
-            dijkstra();
-            cnt = 0;
-            time = 0;
-            for (int i = 1; i <= N; i++) {
-                if (distances[i] != Integer.MAX_VALUE) {
+
+            Arrays.fill(dis,INF);
+
+            dijkstra(c);
+
+            int cnt = 0;
+            int result = 0;
+            for (int i=1; i<=n; i++) {
+                if (dis[i] != INF) {
                     cnt++;
-                    time = Math.max(time, distances[i]);
+                    result = Math.max(result, dis[i]);
                 }
             }
-            System.out.println(cnt + " " + time);
+
+            sb.append(cnt + " " + result + "\n");
+
         }
+
+        System.out.print(sb);
+
     }
 
-    private static void dijkstra() {
-        PriorityQueue<Edge> queue = new PriorityQueue<>();
-        queue.add(new Edge(C, 0));
-        while (!queue.isEmpty()) {
-            Edge edge = queue.poll();
-            int vertex = edge.vertex;
-            int cost = edge.cost;
-            if (distances[vertex] < cost) {
+    private static void dijkstra(int start) {
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        dis[start] = 0;
+
+        while (!pq.isEmpty()) {
+
+            Node node = pq.poll();
+            int dist = node.distance;
+            int now = node.index;
+
+            if (dis[now] < dist) {
                 continue;
             }
-            for (int i = 0; i < edgeList[vertex].size(); i++) {
-                int vertex2 = edgeList[vertex].get(i).vertex;
-                int cost2 = edgeList[vertex].get(i).cost + cost;
-                if (distances[vertex2] > cost2) {
-                    distances[vertex2] = cost2;
-                    queue.add(new Edge(vertex2, cost2));
+
+            for (int i=0; i<graph.get(now).size(); i++) {
+                int cost = dis[now] + graph.get(now).get(i).getDistance();
+
+                if (cost < dis[graph.get(now).get(i).getIndex()]) {
+                    dis[graph.get(now).get(i).getIndex()] = cost;
+                    pq.add(new Node(graph.get(now).get(i).getIndex(), cost));
                 }
             }
+
         }
-    }
 
-    private static class Edge implements Comparable<Edge> {
-        int vertex;
-        int cost;
+    }//dijkstra
 
-        public Edge(int vertex, int cost) {
-            this.vertex = vertex;
-            this.cost = cost;
+    static class Node implements Comparable<Node> {
+        int index, distance;
+        Node (int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+
+        private int getIndex() {
+            return this.index;
+        }
+
+        private int getDistance() {
+            return this.distance;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return cost - o.cost;
+        public int compareTo(Node o) {
+            if (this.distance < o.distance) {
+                return -1;
+            }
+            return 1;
         }
     }
 }
