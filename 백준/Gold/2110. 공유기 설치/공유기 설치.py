48974b61798cec9a06851e18e7196d3dc9f20765
Cha Hyoung Seok <xcohdsuts@gmail.com>
2024-09-27 19:47:04 +0900

    [Gold IV] Title: 공유기 설치, Time: 172 ms, Memory: 40412 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,30 +1,37 @@
 import sys
 
-N, C = map(int, (sys.stdin.readline()).split())
-x, answer = [], 1
-for _ in range(N) :
-    x.append(int(sys.stdin.readline().strip()))
-x = sorted(x)
 
+def valid_install(min_distance):
+    install_count = 1
+    pre_house = house_list[0]
 
-def binary_search(arr, start, end) :
-    if start > end :
-        return
+    for house in house_list :
+        if house - pre_house >= min_distance :
+            install_count += 1
+            pre_house = house
+
+    if install_count >= C :
+        return True
+    else :
+        return False
+
+def binary_search(start, end):
     mid = (start + end) // 2
 
-    current, count = arr[0], 1
-    for i in range(1, N) :
-        if arr[i] >= current + mid :
-            current = arr[i]
-            count += 1
-            
-    global answer
-    if count >= C :
-        answer = mid
-        binary_search(arr, mid + 1, end)
+    if start > end:
+        return mid
+
+    if valid_install(mid):
+        return binary_search(mid + 1, end)
     else :
-        binary_search(arr, start, mid - 1)
+        return binary_search(start, mid - 1)
 
+N, C = map(int, (sys.stdin.readline().strip()).split())
 
-binary_search(x, 1, x[-1] - x[0])
-print(answer)
+house_list = []
+for _ in range(N):
+    house_list.append(int(sys.stdin.readline().strip()))
+
+house_list = sorted(house_list)
+
+print(binary_search(1, max(house_list) // (C - 1)))
