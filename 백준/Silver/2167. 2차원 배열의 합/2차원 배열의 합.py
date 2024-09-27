a6894171f3cf568117087f5c756f662c41700327
Cha Hyoung Seok <xcohdsuts@gmail.com>
2024-09-27 09:38:32 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 100 ms, Memory: 34316 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,22 +1,22 @@
 import sys
 
-N, M = map(int, (sys.stdin.readline()).split())
-board = []
-for _ in range(N) :
-    board.append(list(map(int, (sys.stdin.readline()).split())))
+N, M = map(int, sys.stdin.readline().strip().split())
 
-sum_board = [[0 for _ in range(M + 1)]]
-for i in range(N) :
-    tmp = [0]
-    for j in range(M) :
-        tmp.append(tmp[-1] + board[i][j])
-    sum_board.append(tmp)
+cumulativeSum = [[0 for _ in range(M + 1)]]
+for _ in range(N):
+    cumulativeSum.append([0] + list(map(int, sys.stdin.readline().strip().split())))
 
-for i in range(N) :
-    for j in range(M + 1) :
-        sum_board[i + 1][j] += sum_board[i][j]
-        
+for i in range(N):
+    for j in range(M + 1):
+        cumulativeSum[i + 1][j] = cumulativeSum[i][j] + cumulativeSum[i + 1][j]
+
+for i in range(N + 1):
+    for j in range(M):
+        cumulativeSum[i][j + 1] = cumulativeSum[i][j] + cumulativeSum[i][j + 1]
+
+
 K = int(sys.stdin.readline().strip())
-for _ in range(K) :
-    i, j, x, y = map(int, (sys.stdin.readline()).split())
-    print(sum_board[x][y] + sum_board[i - 1][j - 1] - sum_board[x][j - 1] - sum_board[i - 1][y])
+for _ in range(K):
+    i, j, x, y = map(int, sys.stdin.readline().strip().split())
+    print(cumulativeSum[x][y] - cumulativeSum[i - 1][y] - cumulativeSum[x][j - 1] + cumulativeSum[i - 1][j - 1])
+
