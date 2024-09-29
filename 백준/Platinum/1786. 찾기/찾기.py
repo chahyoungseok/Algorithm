ea34c4ea91db2cf57eb01a15b3381f18179427a5
Cha Hyoung Seok <xcohdsuts@gmail.com>
2024-09-29 15:38:42 +0900

    [Platinum V] Title: 찾기, Time: 520 ms, Memory: 74272 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -1,34 +1,37 @@
 import sys
 
 
-def kmp(allString, pattern) :
+def kmp(all_string, pattern):
     pattern_size = len(pattern)
     table = [0 for _ in range(pattern_size)]
+
     i = 0
-    for j in range(1, pattern_size) :
-        while i > 0 and pattern[i] != pattern[j] :
+    for j in range(1, pattern_size):
+        while i > 0 and pattern[i] != pattern[j]:
             i = table[i - 1]
-        if pattern[i] == pattern[j] :
+        if pattern[i] == pattern[j]:
             i += 1
             table[j] = i
 
     result = []
     i = 0
-    for j in range(len(allString)) :
-        while i > 0 and pattern[i] != allString[j] :
+    for j in range(len(all_string)):
+        while i > 0 and pattern[i] != all_string[j]:
             i = table[i - 1]
-        if pattern[i] == allString[j] :
+        if pattern[i] == all_string[j]:
             i += 1
-            if i == pattern_size :
+            if pattern_size == i:
                 result.append(j - i + 1)
                 i = table[i - 1]
 
     return result
 
 
-allString = input()
-pattern = input()
-result = kmp(allString, pattern)
+T = input()
+P = input()
+
+result = kmp(T, P)
+
 print(len(result))
-for i in result :
-    print(i + 1, end=" ")
+for lo in result :
+    print(lo + 1, end=" ")
