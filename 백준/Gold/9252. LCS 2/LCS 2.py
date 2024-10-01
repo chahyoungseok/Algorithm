61b728ec808421d56fcabe08089c99a41494f26c
Cha Hyoung Seok <xcohdsuts@gmail.com>
2024-10-01 20:32:44 +0900

    [Gold IV] Title: LCS 2, Time: 912 ms, Memory: 424544 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,23 +1,22 @@
-import sys
+string_1 = input()
+string_2 = input()
 
-input1 = sys.stdin.readline().strip()
-input2 = sys.stdin.readline().strip()
+len_1 = len(string_1)
+len_2 = len(string_2)
+lcs = [["" for _ in range(len_2 + 1)] for _ in range(len_1 + 1)]
 
-len_1, len_2 = len(input1), len(input2)
-
-dp = [["" for _ in range(len_2 + 1)] for _ in range(len_1 + 1)]
-
-for i in range(1, len_1 + 1) :
-    for j in range(1, len_2 + 1) :
-        if input1[i - 1] == input2[j - 1] :
-            dp[i][j] = dp[i - 1][j - 1] + input1[i - 1]
-        else :
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=lambda x : len(x))
+for i in range(1, len_1 + 1):
+    for j in range(1, len_2 + 1):
+        if string_1[i - 1] == string_2[j - 1]:
+            lcs[i][j] = lcs[i - 1][j - 1] + string_1[i - 1]
+        else:
+            lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1], key=lambda x:len(x))
 
 result = ""
-for i in range(len_1 + 1) :
-    result = max(result, max(dp[i], key=lambda x : len(x)), key=lambda x : len(x))
-    
-print(len(result))
-if len(result) != 0 :
+for i in range(1, len_1 + 1):
+    result = max(result, max(lcs[i], key=lambda x : len(x)), key=lambda x : len(x))
+
+result_len = len(result)
+print(result_len)
+if result_len != 0:
     print(result)
