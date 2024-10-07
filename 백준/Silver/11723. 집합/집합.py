a8dfbd7f28ab469ff3c7fd42c9c70d121f5875dd
Cha Hyoung Seok <xcohdsuts@gmail.com>
2024-10-07 14:11:02 +0900

    [Silver V] Title: 집합, Time: 4080 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,27 +1,25 @@
 import sys
 
-
-M = int(input())
+M = int(sys.stdin.readline().strip())
 S = 0
-for _ in range(M) :
-    data = list(map(str, sys.stdin.readline().split()))
-    if data[0] == "add" :
-        if S & (1 << int(data[1])) == 0 :
-            S |= (1 << int(data[1]))
-    elif data[0] == "remove" :
-        if S & (1 << int(data[1])) != 0 :
-            S &= ~(1 << int(data[1]))
-    elif data[0] == "check" :
-        if S & (1 << int(data[1])) != 0:
-            print(1)
-        else :
+for _ in range(M):
+    data = list(map(str, sys.stdin.readline().strip().split()))
+    if data[0] == "add":
+        S |= 1 << int(data[1])
+    elif data[0] == "remove":
+        S &= ~(1 << int(data[1]))
+    elif data[0] == "check":
+        if S & (1 << int(data[1])) == 0:
             print(0)
+        else:
+            print(1)
     elif data[0] == "toggle":
-        if S & (1 << int(data[1])) != 0:
-            S &= ~(1 << int(data[1]))
-        else :
+        if S & (1 << int(data[1])) == 0:
             S |= (1 << int(data[1]))
+        else:
+            S &= ~(1 << int(data[1]))
     elif data[0] == "all":
-        S = (1 << 21) - 1
-    else :
+        S = 2**21 - 1
+    elif data[0] == "empty":
         S = 0
+
